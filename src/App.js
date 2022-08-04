d2fc3f9efe9d70b5184ee99346950e6ed919329c
FlorDiMarco <flor.di.marco.95@gmail.com>
2022-08-04 00:38:21 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-
+import React from "react";
+import { BrowserRouter, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css"
+
 function App() {
   return (
-    <div >
-      <Footer/>
-
-      
-      
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
