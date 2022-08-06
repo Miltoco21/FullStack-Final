44b7bd5330a1944e1d9011a7feac73a938746a79
JM2311 <juan_morales2311@hotmail.com>
2022-08-06 20:48:42 -0300

    ultimando detalles

--- src/pages/PaginaDetalle.jsx
+++ src/pages/PaginaDetalle.jsx
@@ -29,58 +29,74 @@ const PaginaDetalle = () => {
   return (
     <>
       {isLoader ? (
-        <div className={styles.detalleContainer}>
-          <img
-            className={`${styles.col} ${styles.detalleImagen}`}
-            src={movie.poster}
-            alt={movie.titulo}
-          />
-          <div className={styles.col}>
-            <p className={styles.firstItem}>
-              <strong>Titulo: </strong>
-              {movie.titulo}
-            </p>
-            <p>
-              <strong>Descripcion:</strong> {movie.descripcion}
-            </p>
-            <p>
-              <strong>Genero:</strong>
-              {movie.genero}
-            </p>
-            <p>
-              <strong>Fecha de Estreno: </strong>
-              {movie.fecha}
-            </p>
-            <form>
-              <h2>Deja tu comentario:</h2>
-              <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  class="form-control"
-                  id="exampleFormControlInput1"
-                  placeholder="name@example.com"
-                  required
-                />
-              </div>
-              <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">
-                  Reseña
-                </label>
-                <textarea
-                  class="form-control"
-                  id="exampleFormControlTextarea1"
-                  rows="3"
-                ></textarea>
-              </div>
-              <button type="submit" class="btn btn-primary">
-                Enviar
-              </button>
-            </form>
+        <>
+          <div className={styles.detalleContainer}>
+            <img
+              className={`${styles.col} ${styles.detalleImagen}`}
+              src={movie.poster}
+              alt={movie.titulo}
+            />
+            <div className={styles.col}>
+              <p className={styles.firstItem}>
+                <strong>Titulo: </strong>
+                {movie.titulo}
+              </p>
+              <p>
+                <strong>Descripcion:</strong> {movie.descripcion}
+              </p>
+              <p>
+                <strong>Genero: </strong>
+                {movie.genero.join(", ")}
+              </p>
+              <p>
+                <strong>Fecha de Estreno: </strong>
+                {movie.fecha}
+              </p>
+              <form>
+                <h2>Deja tu comentario:</h2>
+                <div class="mb-3">
+                  <label for="exampleFormControlInput1" class="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="name@example.com"
+                    required
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleFormControlTextarea1" class="form-label">
+                    Reseña
+                  </label>
+                  <textarea
+                    class="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                  ></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">
+                  Enviar
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
+          <div className={`${styles.rowTrailer} row justify-content-center align-items-center flex-column my-4`}>
+            <h2 className="text-light text-center fw-bold my-5">
+              Trailer Oficial
+            </h2>
+            <div className={`${styles.containerVideo} col`}>
+              <iframe
+                className={`${styles.video} rounded`}
+                src={movie.trailer}
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              ></iframe>
+            </div>
+          </div>
+        </>
       ) : (
         <Loader />
       )}
