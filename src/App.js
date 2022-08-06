f539220236f6d5945ddc060493ae82eddb775e2d
JM2311 <juan_morales2311@hotmail.com>
2022-08-06 16:51:16 -0300

    arreglando detalles del navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css"
 
@@ -11,7 +10,6 @@ function App() {
       <Header />
       <Routes>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,31 +1,52 @@
 import React from "react";
-import { Navbar, Container, Nav, NavDropdown, Form, Button, Image } from "react-bootstrap";
-import { Search } from 'react-bootstrap-icons';
-import logito from "../../assets/img/logito.png"
-
+import { Navbar, Container, Nav, Button, Image } from "react-bootstrap";
+import logo from "../../assets/img/logoStrangerFlix.png";
+import styles from "../Header/Header.module.css";
 function Header() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="/"><Image src={logito} alt="logito" width={150}/></Navbar.Brand>
+        <Navbar.Brand href="/">
+          <Image src={logo} alt="logito" width={150} />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Form className="d-flex mx-3">
-              <Form.Control
-                type="search"
-                placeholder=" Buscar"
-                className="me-2 rounded-pill"
-                aria-label="Search"
-              />
-              <Button variant="outline-danger rounded-pill pt-0"><Search /></Button>
-            </Form>
-            <Button variant="danger" className="rounded-pill ms-1 me-2">Iniciar sesion</Button>
-            <Button variant="danger" className="rounded-pill ms-1 me-2">Registrarse</Button>
-            <NavDropdown title="Categorias" id="basic-nav-dropdown" className="me-5">
-              <NavDropdown.Item className="text-danger fontbold" href="/">Peliculas</NavDropdown.Item>
-              <NavDropdown.Item className="text-danger fontbold" href="#action/3.4">Series</NavDropdown.Item>
-            </NavDropdown>
+            <div className={styles.containerButtons}>
+              <Button
+                variant="danger"
+                className={`${styles.botonSesion} rounded-pill ms-1 me-2`}
+              >
+                Iniciar sesion
+              </Button>
+              <Button
+                variant="danger"
+                className={`${styles.botonRegistro} rounded-pill ms-1 me-2`}
+              >
+                Registrarse
+              </Button>
+              <Button
+                variant="danger"
+                className={`${styles.botonCategorias} rounded-pill ms-1 me-2`}
+              >
+                Categorias
+              </Button>
+            </div>
+            {/* <NavDropdown
+              title="Categorias"
+              id="basic-nav-dropdown"
+              className="me-5"
+            >
+              <NavDropdown.Item className="text-danger fontbold" href="/">
+                Peliculas
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                className="text-danger fontbold"
+                href="#action/3.4"
+              >
+                Series
+              </NavDropdown.Item>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
